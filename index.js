03a64545055cbb1f3999e357eed26623969d7ce3
Warren Santiago <wsantiago9623@gmail.com>
2022-09-25 00:25:14 -0400

    commit tree 63e23bc67d04abd7bdf5a6982b92d5658259a186

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(somVal){
+    if (somVal>42)
+    return (somVal-42)
+    if (somVal<42)
+    return (42-somVal)
+}
+
+function distanceFromHqInFeet(somVal){
+    distanceFromHqInBlocks(somVal)
+    if (somVal>42)
+    return ((somVal-42)*264)
+    if (somVal<42)
+    return ((42-somVal)*264)
+}
+
+function distanceTravelledInFeet(val1, val2){
+    if(val1 > val2){
+        return (val1 - val2) * 264;
+    } else if (val1 < val2) {
+        return (val2 - val1) *264;
+    }
+
+}
+
+
+function calculatesFarePrice(val1, val2){ 
+    let calcDistance = distanceTravelledInFeet(val1, val2);
+    let fare = 0;
+
+    if (calcDistance > 400 && calcDistance < 2000){
+        let fareDistance = calcDistance - 400; 
+        fare = fareDistance * .02;
+    } else if (calcDistance > 2000 && calcDistance < 2500){
+        fare = 25;
+    } else if (calcDistance > 2500){
+        return "cannot travel that far";
+    }
+
+    return fare;
+
+
+
+
+    
+
+ 
+
+
+
+
+}
